9dd78657fe4821f49accacd2f62b5f2fdeda13b9
yaswanthreddy1007 <164986952+yaswanthreddy1007@users.noreply.github.com>
2024-05-28 12:54:39 +0530

    Update ec2-creation.tf

--- terraformscripts/ec2-creation.tf
+++ terraformscripts/ec2-creation.tf
@@ -1,9 +1,11 @@
-provider "aws"{
+provider "aws"
+{
   region="ap-south-1"
 }
 
-resource "aws_instance" "AWSEC2INSTANCE "{
-  ami="0cc9838aa7ab1dce7"
+resource "aws_instance" "AWSEC2INSTANCE "
+{
+  ami="ami-0cc9838aa7ab1dce7"
   instance_type= "t2.micro"
   key_name= "devopsmarch6batch.pem"
   security_group=["launch-wizard-12"]
